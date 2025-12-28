72f9d7cf8c4452217069e0c238e6dcfaff2300b5
Aditya Singh <1adityasingh@proton.me>
2025-12-28 14:30:05 +0530

    Deployed 2d1cb92 with MkDocs version: 1.6.1

--- quickstart/03_04_subcommands.py
+++ quickstart/03_04_subcommands.py
@@ -1,28 +1,41 @@
-from typing import Optional
-
 import clap
+from clap import arg
 
 
 @clap.subcommand
 class Add:
     """Adds files to myapp."""
 
-    name: Optional[str]
+    name: str = arg()
+
+
+@clap.subcommand
+class Remove:
+    """Remove files from myapp."""
+
+    name: str = arg()
 
 
 @clap.command(version="1.0", propagate_version=True)
 class Cli(clap.Parser):
-    command: Add
+    command: Add | Remove
+    # Alternatively, you can write command: Union[Add, Remove]
+
+    quiet: bool = arg(short=True, long=True)
 
 
 def main():
-    cli = Cli.parse()
+    args = Cli.parse()
 
     # You can check for the existence of subcommands, and if found use their
     # matches just as you would the top level cmd
-    match cli.command:
+    match args.command:
         case Add(name):
-            print(f"'myapp add' was used, name is: {name}")
+            if not args.quiet:
+                print(f"'myapp add' was used, name is: {name}")
+        case Remove(name):
+            if not args.quiet:
+                print(f"'myapp remove' was used, name is: {name}")
 
 
 if __name__ == "__main__":
